5c17fd3606078dee11e58b2cd1d38a2ea62541e9
Tara Singh Sethi <tarasinghsethi@hotmail.com>
2026-04-01 13:58:00 +0100

    Update tag

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ region = "ap-south-2"
 resource "aws_s3_bucket" "demo_bucket" {
 bucket = "my-terraform-cicd-demo-bucket-12345"
 tags = {
-Name = "Updated Terraform CICD Demo"
+Name = "Terraform CICD Demo"
 }
 }
