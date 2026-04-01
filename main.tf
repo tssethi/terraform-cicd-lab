233a4b79151c3733b43c8cafaeae4eebbcd3f902
Tara Singh Sethi <tarasinghsethi@hotmail.com>
2026-04-01 13:51:06 +0100

    Update tag

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
+terraform {
+backend "s3" {
+bucket = "your-terraform-state-bucket"
+key = "terraform.tfstate"
+region = "ap-south-2"
+}
+}
 provider "aws" {
 region = "ap-south-2"
 }
+
 resource "aws_s3_bucket" "demo_bucket" {
 bucket = "my-terraform-cicd-demo-bucket-12345"
 tags = {
